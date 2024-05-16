74764ad616ed1e530afe5ee5c68c4c7d7e24f098
Mayank Kumar <mayunksingh2000@gmail.com>
2024-05-16 23:21:13 +0530

    login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation 'com.hbb20:android-country-picker:2.5.0'
 }
