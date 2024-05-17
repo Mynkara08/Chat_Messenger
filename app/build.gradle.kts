50fff85a8ac6fb2e77ee3397c7ecaa78dced1968
Mayank Kumar <mayunksingh2000@gmail.com>
2024-05-17 17:11:03 +0530

    firebase setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -42,8 +44,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.country.code.picker)
+    implementation(platform(libs.firebase.bom.code))
 }
--- app/src/main/java/mynk/ara/chat_messenger/LoginNumberActivity.kt
+++ app/src/main/java/mynk/ara/chat_messenger/LoginNumberActivity.kt
@@ -1,12 +1,26 @@
 package mynk.ara.chat_messenger
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hbb20.CountryCodePicker
 
 class LoginNumberActivity : AppCompatActivity() {
+    var countryCodePicker: CountryCodePicker? = null
+    var phoneInput: EditText? = null
+    var sendOtpBtn: Button? = null
+    var progressBar: ProgressBar? = null
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +29,29 @@ class LoginNumberActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+        countryCodePicker = findViewById(R.id.login_countrycode)
+        phoneInput = findViewById(R.id.login_mobile_number)
+        sendOtpBtn=findViewById(R.id.sent_otp_btn)
+        progressBar=findViewById(R.id.login_progress_bar)
+        countryCodePicker?.registerCarrierNumberEditText(phoneInput)
+        progressBar?.visibility = View.GONE
+
+        sendOtpBtn?.setOnClickListener {
+            if (!countryCodePicker?.isValidFullNumber()!!) {
+                phoneInput?.setError("Phone number not valid")
+                return@setOnClickListener
+            }
+            val intent = Intent(this@LoginNumberActivity, LoginOtpActivity::class.java)
+            startActivity(intent)
+            intent.putExtra("phone", countryCodePicker?.fullNumberWithPlus ?: "")
+            startActivity(intent)
+
+
+        }
+
+
     }
 }
