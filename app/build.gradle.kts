b4915e76e4058c2deabb21a37e66b792ee1d1de8
Mayank Kumar <mayunksingh2000@gmail.com>
2024-05-17 11:27:17 +0530

    fix login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation 'com.hbb20:android-country-picker:2.5.0'
+    implementation(libs.country.code.picker)
 }
--- app/src/main/java/mynk/ara/chat_messenger/Splash_Activity.kt
+++ app/src/main/java/mynk/ara/chat_messenger/Splash_Activity.kt
@@ -1,10 +1,12 @@
 package mynk.ara.chat_messenger
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class Splash_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,10 @@ class Splash_Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        android.os.Handler().postDelayed({
+            startActivity(Intent(this@Splash_Activity,LoginNumberActivity::class.java))
+            finish()
+        },2000)
+
     }
 }
